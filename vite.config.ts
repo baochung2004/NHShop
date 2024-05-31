2d1a4ab1962dd19e632a34c89ad48034032811fc
BaoChung-Cloud <chungkbph38477@fpt.edu.vn>
2024-05-31 15:22:52 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,1 @@
-// vite.config.js
-import { resolve } from "path";
-import { defineConfig } from "vite";
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        products: resolve(__dirname, "src/products.html")
-      }
-    }
-  }
-});
